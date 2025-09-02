b9125cf95268c247cd7cf362d298ff0ba454e70b
Vadim-Galimov <115936408+Vadim-Galimov@users.noreply.github.com>
2025-09-02 14:02:09 +0500

    add pages
    empty templates for about and help. first run pages

--- main/views.py
+++ main/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("тут будет главная страница")
+
+def about(request):
+    return HttpResponse("Тут будет страница о приюте")
+
+def help(request):
+    return HttpResponse("Тут будет страница как помочь приюту")
